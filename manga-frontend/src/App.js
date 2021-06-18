faafb03ffd578f2cc087e7b3c071b6a7cb574557
Gamoundo <akilgg@gmail.com>
2021-06-18 17:00:32 -0400

    made a jank filter

--- manga-frontend/src/App.js
+++ manga-frontend/src/App.js
@@ -8,6 +8,39 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import User from './User';
 
 function App() {
+
+const posts = [
+  {
+    userName: "Joe",
+    title: "Samurai manga?",
+    content: "anyone remembers a manga where a samurai is fighting a mechanical bear? Can't remember any other details.",
+    responses: []
+  },
+  {
+    userName: "Sara",
+    title: "Gun fu?",
+    content: "Any mangas with some great gun kata?",
+    responses: []
+  }
+
+]
+
+let discussions = [
+  {
+    userName: "Joe",
+    title: "looking for a new manga",
+    content: "Just finished reading casshern sins, can someone suggest something similar?",
+    responses: []
+  }
+]
+
+let uP = []
+let uD = []
+uP.push(posts.filter(post => post.userName === "Joe"))
+uD.push(discussions.filter(discus => discus.userName === "Joe"))
+
+
+
   return (
     <Router>
     <div className="App">   
@@ -18,7 +51,14 @@ function App() {
       <Route exact path= '/posts' component ={Posts}/>
       <Route exact path= '/discussions' component ={Discussions}/>
       <Route exact path= '/manga' component ={Manga}/>
-      <Route exact path= '/user' component ={User}/>
+      <Route exact path= '/user'  render={routeProps => (
+        <User 
+        {...routeProps}
+        userPosts={uP}
+        userDisc={uD}
+        />
+        )}
+        />
     </div>
     </Router>
     
--- manga-frontend/src/User.js
+++ manga-frontend/src/User.js
@@ -3,14 +3,16 @@ import React from 'react'
 class User extends React.Component {
 
     state = {
-        userPosts: [],
+        userPosts: this.props.userPosts || "nothing to see here.",
         userName: 'Joe',
         userDiscus: []
     }
     render() {
+        console.log(this.props)
         return (
         <div className="userPage">
             <h3>{this.state.userName}'s posts and discussions</h3>
+            <h2>{this.state.userPosts}</h2>
         </div>
 
         
